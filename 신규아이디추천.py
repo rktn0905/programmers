53631deb55ce4773cf156b20b8ab63c072a9b57d
Taeyong Choi <50408465+rktn0905@users.noreply.github.com>
2021-02-14 22:04:45 +0900

    Update 신규아이디추천.py

--- 신규아이디추천.py
+++ 신규아이디추천.py
@@ -4,11 +4,8 @@ def solution(new_id):
     answer = ''
     answer = new_id.lower()
     
-    answer = re.sub('[~!@#$%^&*()=+:?, <>/]','',answer)
-    answer = answer.replace('{','')
-    answer = answer.replace(']','')
-    answer = answer.replace('}','')
-    answer = answer.replace('[','')
+    answer = re.sub('[^a-z0-9\-_.]','',answer)
+
     while answer.find('..')!= -1 :
         answer = answer.replace('..','.')
     
@@ -16,8 +13,7 @@ def solution(new_id):
     
     answer = 'a' if answer == '' else answer
     
-    answer = answer[0:15]
-    answer = answer.strip('.')
+    answer = answer[0:15].strip('.')
     
     answer = answer + answer if len(answer) < 2 else answer 
     answer = answer + answer[1] if len(answer) < 3 else answer 
